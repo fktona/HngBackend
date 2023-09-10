df525e6aeb7e5ad1680e38f0b45cb08c7523b3e0
fktona <adetona.fk@gmail.com>
2023-09-11 00:04:59 +0100

    dones

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/api', (req, res) => {
   // Get the current date and time
   const currentDate = new Date();
   const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
-  const utcTime = currentDate.toISOString();
+  const utcTime = const utcTime = currentDate.toISOString().slice(0, -1);;
 
   // Construct the JSON response
   const response = {
